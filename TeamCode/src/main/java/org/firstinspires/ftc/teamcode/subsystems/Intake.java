c9b09bd0d13db9cd2c2fe81c817e440c979990a4
AbeetoChanito <abeetochan@gmail.com>
2025-11-12 10:26:04 -0500

    add more intake logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,31 +1,57 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @Config
 public class Intake {
     private Motor motor;
+    private SimpleServo gate;
+
+    // intake configuration
+    private static double inPower = 1.0;
+    private static double outPower = -1.0;
+    private static double storePower = 0.15;
+
+    // gate configuration
+    private static double gateOpen = 0.0;
+    private static double gateClosed = 0.0;
 
     public Intake(OpMode opMode) {
         motor = new Motor(opMode.hardwareMap, "intake");
         motor.setRunMode(Motor.RunMode.RawPower);
+        gate = new SimpleServo(opMode.hardwareMap, "gate", 0, 360, AngleUnit.DEGREES);
     }
 
-    public void setVoltage(double voltage) {
-        motor.set(voltage);
+    public void setPower(double power) {
+        motor.set(power);
     }
 
     public void in() {
-        setVoltage(1.0);
+        setPower(inPower);
     }
 
     public void out() {
-        setVoltage(-1.0);
+        setPower(outPower);
+    }
+
+    public void store() {
+        setPower(storePower);
     }
 
     public void stop() {
-        setVoltage(0.0);
+        setPower(0.0);
+    }
+
+    public void openGate() {
+        gate.setPosition(gateOpen);
+    }
+
+    public void closeGate() {
+        gate.setPosition(gateClosed);
     }
 }
