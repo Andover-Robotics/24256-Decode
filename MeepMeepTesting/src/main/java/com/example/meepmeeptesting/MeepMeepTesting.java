1503bbbf26a9658c94aca06e60d0e7d4213494b9
AbeetoChanito <132693471+AbeetoChanito@users.noreply.github.com>
2025-12-22 20:46:13 -0500

    12 ball + open gate (top 1 thing that will never happen to sandstorm)

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -10,16 +10,53 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(600);
 
+        // width 338mm
+        // height 442mm
+
         RoadRunnerBotEntity bot = new DefaultBotBuilder(meepMeep)
                 .setConstraints(
                         60,
                         60,
                         Math.toRadians(180),
                         Math.toRadians(180),
-                        15
+                        12
                 )
-                .setDimensions(18, 18)
+                .setDimensions(13.3071, 17.4016)
                 .build();
+
+        bot.runAction(
+                bot.getDrive()
+                        // start on field wall
+                        .actionBuilder(new Pose2d(-72 + 17.4016/2, -24, Math.toRadians(180)))
+                        // starting load
+                        .strafeToLinearHeading(new Vector2d(-24, -24), Math.toRadians(225))
+                        .setTangent(0)
+
+                         // pickup 1
+                        .strafeToLinearHeading(new Vector2d(-12, -24 - 4), Math.toRadians(-90))
+                        .strafeToLinearHeading(new Vector2d(-12, -48), Math.toRadians(-90))
+
+                        // open gate
+                        .strafeToLinearHeading(new Vector2d(0, -56), Math.toRadians(180))
+                        .waitSeconds(3)
+
+                        // shoot 1
+                        .strafeToLinearHeading(new Vector2d(-24, -24), Math.toRadians(225))
+
+
+                        // pickup 2 -> shoot 2
+                        .setTangent(0)
+                        .strafeToLinearHeading(new Vector2d(12, -24 - 4), Math.toRadians(-90))
+                        .strafeToLinearHeading(new Vector2d(12, -48), Math.toRadians(-90))
+                        .strafeToLinearHeading(new Vector2d(-24, -24), Math.toRadians(225))
+
+                        .setTangent(0)
+                        .strafeToLinearHeading(new Vector2d(36, -24 - 4), Math.toRadians(-90))
+                        .strafeToLinearHeading(new Vector2d(36, -48), Math.toRadians(-90))
+                        .strafeToLinearHeading(new Vector2d(-24, -24), Math.toRadians(225))
+
+                        .build()
+        );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
                 .setDarkMode(true)
